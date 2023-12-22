17dcbf11e67adcf531863c1016289c4a9ee588b4
Darya Balba <dpbalba@edu.hse.ru>
2023-12-22 17:40:52 +0300

    create sentences file and change book

--- src/sentences.py
+++ src/sentences.py
@@ -11,5 +11,15 @@ class SentenceSyntax:
         self.sentence = sentence
         nlp = spacy.load("ru_core_news_sm")
         self.doc = nlp(sentence)
+        self.root, self.root_morph = self.get_root_morph()
 
+    def get_root_morph(self):
+        """
+        get sentence root and its morph characteristics
+        :return: text of root, morph characteristic of root
+        """
+        for token in self.doc:
+            if token.dep_ == 'ROOT':
+                return token.text, token.morph
+                # мне каж нам надо доставать морфологические характеристики по другому. например сделать
 # короче хз че тут еще добавить, думаю насчет создания списков морф критериев и т.д
