a2e5d566bb2fe82ecbd79f8c11a8aaaff99d622e
Darya Balba <dpbalba@edu.hse.ru>
2023-12-22 21:16:01 +0300

    add rules

---
+++ src/rules.py
@@ -0,0 +1,31 @@
+class Rules:
+    def __init__(self, sentence):
+        self.sentence = sentence
+
+    def check_nominative(self):
+        """
+        ифы для номинатива
+        :return: True or False
+        """
+        pass
+
+    def check_genitive(self):
+        """
+        ифы для генитива
+        :return: True or False
+        """
+        pass
+
+    def check_vocative(self):
+        """
+        ифы для вокатива
+        :return: True or False
+        """
+        pass
+
+    def check_infinitive(self):
+        """
+        ифы для инфинитивных
+        :return: True or False
+        """
+        pass
