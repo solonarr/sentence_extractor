6c5e2c12bb06319631fd2ea807432b23e5ff2b59
christine-ni <kristinanigamedzanova@gmail.com>
2023-12-23 03:31:48 +0300

    типа тесты

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,10 +1,45 @@
 import unittest
-
+from src.rules import Rules
+from src.sentences import SentenceSyntax
 
 class RulesBaseTests(unittest.TestCase):
 
     def test_check_nominative(self):
         self.assertEqual(True, False)
+
+
+
+    def test_check_defpersonal_ind(self):
+        text = 'Пойдем со мной.'
+        sentence = SentenceSyntax(text)
+        rule = Rules(sentence)
+        result = rule.check_defpersonal()
+        self.assertEqual(result, True)
+
+    def test_check_defpersonal_impr(self):
+        text = 'Принеси отцу пива.'
+        sentence = SentenceSyntax(text)
+        rule = Rules(sentence)
+        result = rule.check_defpersonal()
+        self.assertEqual(result, True)
+
+
+
+
+    def test_check_vagpersonal_in_pres(self):
+        text = 'Тебя зовут гулять.'
+        sentence = SentenceSyntax(text)
+        rule = Rules(sentence)
+        result = rule.check_vagpersonal()
+        self.assertEqual(result, True)
+
+    def test_check_vagpersonal_in_past(self):
+        text = 'В дверь позвонили.'
+        sentence = SentenceSyntax(text)
+        rule = Rules(sentence)
+        result = rule.check_vagpersonal()
+        self.assertEqual(result, True)
+
 
 
 if __name__ == '__main__':
