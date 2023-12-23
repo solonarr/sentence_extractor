f92da783246959aabafe710c2c0695f073c68fcc
christine-ni <kristinanigamedzanova@gmail.com>
2023-12-23 04:25:46 +0300

    added  tests

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -62,5 +62,34 @@ class RulesBaseTests(unittest.TestCase):
         result = rule.check_impersonal()
         self.assertEqual(result, True)
 
+
+    def test_check_defpersonal_ind(self):
+        text = 'Пойдем со мной.'
+        sentence = SentenceSyntax(text)
+        rule = Rules(sentence)
+        result = rule.check_defpersonal()
+        self.assertEqual(result, True)
+
+    def test_check_defpersonal_impr(self):
+        text = 'Принеси отцу пива.'
+        sentence = SentenceSyntax(text)
+        rule = Rules(sentence)
+        result = rule.check_defpersonal()
+        self.assertEqual(result, True)
+
+    def test_check_vagpersonal_in_pres(self):
+        text = 'Тебя зовут гулять.'
+        sentence = SentenceSyntax(text)
+        rule = Rules(sentence)
+        result = rule.check_vagpersonal()
+        self.assertEqual(result, True)
+
+    def test_check_vagpersonal_in_past(self):
+        text = 'В дверь позвонили.'
+        sentence = SentenceSyntax(text)
+        rule = Rules(sentence)
+        result = rule.check_vagpersonal()
+        self.assertEqual(result, True)
+
 if __name__ == '__main__':
     unittest.main()
